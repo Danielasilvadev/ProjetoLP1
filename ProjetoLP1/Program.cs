aadb58507db8bc06033c347e20ac0872b3941920
RodrigoPiresgames <Rodrigo.Pires.gamesdesign@gmail.com>
2022-03-31 13:18:26 +0100

    Fix Random generator

--- ProjetoLP1/Program.cs
+++ ProjetoLP1/Program.cs
@@ -5,11 +5,17 @@ namespace ProjectLP1
     class Program
     {
 
-        public int RandNum(int x, int y) => rnd.Next(x, y);
+        public int RandNum(int x, int y)
+        {
+            Random rnd = new Random();
+            int num =rnd.Next(x, y);
+            return num;
+        }
+        public int [][] board;
 
         public int[][] BoardCreation()
         {
-            int [][] board  = new int [5][];
+            board  = new int [5][];
             board [4] = new int [] {0, 0, 0, 0, 0};
             board [3] = new int [] {0, 0, 0, 0, 0};
             board [2] = new int [] {0, 0, 0, 0, 0};
@@ -21,68 +27,68 @@ namespace ProjectLP1
             board[0][0] = 1;
             board[5][5] = 1;
 
-            int snake_c = rnd.Next(0, 5);
-            int snake_l = rnd.Next(1, 5);
+            int snake_c = RandNum(0, 5);
+            int snake_l = RandNum(1, 5);
 
-            int ladders_c = rnd.Next(0, 5);
-            int ladders_l = rnd.Next(0, 4);
+            int ladders_c = RandNum(0, 5);
+            int ladders_l = RandNum(0, 4);
 
-            int Cobra_c = rnd.Next(0, 5);
-            int Cobra_l = rnd.Next(2, 5);
+            int Cobra_c = RandNum(0, 5);
+            int Cobra_l = RandNum(2, 5);
 
-            int Boost_c = rnd.Next(0, 5);
-            int Boost_l = rnd.Next(0, 4);
+            int Boost_c = RandNum(0, 5);
+            int Boost_l = RandNum(0, 4);
 
-            int UTurn_c = rnd.Next(0, 5);
-            int UTurn_l = rnd.Next(1, 5);
+            int UTurn_c = RandNum(0, 5);
+            int UTurn_l = RandNum(1, 5);
                 
-            int ExtraDie_c = rnd.Next(0, 5);
-            int ExtraDie_l = rnd.Next(0, 5);
+            int ExtraDie_c = RandNum(0, 5);
+            int ExtraDie_l = RandNum(0, 5);
 
-            int CheatDie_c = rnd.Next(0, 5);
-            int CheatDie_l = rnd.Next(0, 5);
+            int CheatDie_c = RandNum(0, 5);
+            int CheatDie_l = RandNum(0, 5);
 
             board [snake_c][snake_l] = 2;
 
             while (board [ladders_c][ladders_l] != 0)
             {
-                ladders_c = rnd.Next(0, 5);
-                ladders_l = rnd.Next(0, 4);
+                ladders_c = RandNum(0, 5);
+                ladders_l = RandNum(0, 4);
             }
             board [ladders_c][ladders_l] = 3;
 
             while (board [Cobra_c][Cobra_l] != 0)
             {
-                Cobra_c = rnd.Next(0, 5);
-                Cobra_l = rnd.Next(2, 5);
+                Cobra_c = RandNum(0, 5);
+                Cobra_l = RandNum(2, 5);
             }
             board [Cobra_c][Cobra_l] = 4;
 
             while (board [Boost_c][Boost_l] != 0)
             {
-                Boost_c = rnd.Next(0, 5);
-                Boost_l = rnd.Next(0, 4);
+                Boost_c = RandNum(0, 5);
+                Boost_l = RandNum(0, 4);
             }
             board [Boost_c][Boost_l] = 5;
 
             while (board [Boost_c][Boost_l] != 0)
             {
-                UTurn_c = rnd.Next(0, 5);
-                UTurn_l = rnd.Next(1, 5);
+                UTurn_c = RandNum(0, 5);
+                UTurn_l = RandNum(1, 5);
             }
             board [UTurn_c][UTurn_l] = 6;
 
             while (board [ExtraDie_c][ExtraDie_l] != 0)
             {
-                ExtraDie_c = rnd.Next(0, 5);
-                ExtraDie_l = rnd.Next(0, 5);
+                ExtraDie_c = RandNum(0, 5);
+                ExtraDie_l = RandNum(0, 5);
             }
             board [ExtraDie_c][ExtraDie_l] = 7;
 
             while (board [CheatDie_c][CheatDie_l] != 0)
             {
-                CheatDie_c = rnd.Next(0, 5);
-                CheatDie_l = rnd.Next(0, 5);
+                CheatDie_c = RandNum(0, 5);
+                CheatDie_l = RandNum(0, 5);
             }
             board [CheatDie_c][CheatDie_l] = 8;
 
@@ -95,7 +101,7 @@ namespace ProjectLP1
         {
             BoardCreation();
             int player_pos = 0;
-            int dice = rnd.Next(1, 7);
+            int dice = RandNum(1, 7);
             player_pos += dice;
             Console.WriteLine($"{board}");
 
